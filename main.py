8918ecea9588926d4ed7802936d97dd532db6413
Shane Josapak <shjosapak@gmail.com>
2021-09-26 18:57:44 +0000

    demo for Tuesday

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = flask.Flask(__name__)
 def root():
     return flask.render_template('index.html')
 
-@app.route('/homepage.html')
-def homepage():
-    return flask.render_template('homepage.html')
+@app.route('/p/<requested_page>')
+def templater(requested_page):
+    return flask.render_template(requested_page)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
