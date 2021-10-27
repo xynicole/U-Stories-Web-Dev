234f6db3883e29c967272eeb6c166b86aa1628a7
Shane Josapak <shjosapak@gmail.com>
2021-10-27 15:07:00 +0000

    fixed more typos, added some form of receive story function, still needs to be fleshed out

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ def create_new_story():
     update_entries(story_list)
     return flask.render_template('homepage.html')
 
+''' Probably don't actually need this method maybe
 @app.route('/get-story', methods=['POST', 'GET'])
 def get_story():
     id = flask.request.values['id']
     datastore_story_entry = retrieve_story(id)
-        
+'''
 
 # ---------- Actual Web Pages Start Here ----------
 @app.route('/')
@@ -37,6 +38,14 @@ def root():
 def write_story():
     return flask.render_template('write-story.html')
 
+@app.route('/p/receive-story.html', methods=['POST', 'GET'])
+def receive_story():
+    id = flask.request.values['id']
+    datastore_story_entry = retrieve_story(id)
+    return flask.render_template('receive-story.html', story_list=datastore_story_entry)
+
+
+# Any page that is not specified will default here with no functionality
 @app.route('/p/<requested_page>')
 def templater(requested_page):
     return flask.render_template(requested_page)
