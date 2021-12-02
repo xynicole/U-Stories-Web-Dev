57c6a6aac141db50bb35720891ad2107c84da675
Shane Josapak <shjosapak@gmail.com>
2021-12-02 22:01:07 +0000

    enhanced datastore. fixed visual components

--- datastore.py
+++ datastore.py
@@ -1,6 +1,8 @@
 import flask
 import datetime
 
+import os
+import socket
 import sys
 import random
 
@@ -21,28 +23,32 @@ def get_head_stories():
     story_list = []
     client = get_client()
     query = client.query(kind='head_story')
-    for entity in query.fetch():
+    for entity in query.fetch():        
         story_list.append(entity)
     return story_list
 
 def create_story():
     client = get_client()
-    key = client.key('child_story', random.randint(1, sys.maxsize))
+    name = socket.gethostbyname(socket.gethostname()) + str(datetime.datetime.now()) + str(random.randint(1, sys.maxsize))
+    key = client.key('child_story', name)
     return datastore.Entity(key)
 
 def create_head_story():
     client = get_client()
-    key = client.key('head_story', random.randint(1, sys.maxsize))
+    name = socket.gethostbyname(socket.gethostname()) + str(datetime.datetime.now()) + str(random.randint(1, sys.maxsize))
+    key = client.key('head_story', name)
+    print(key.name)
     return datastore.Entity(key)
 
-def retrieve_head_story(id):
+def retrieve_head_story(name):
+    print("retrieve " + name)
     client = get_client()
-    key = client.key('head_story', int(id))
+    key = client.key('head_story', name)
     return client.get(key)
 
-def retrieve_story(id):
+def retrieve_story(name):
     client = get_client()
-    key = client.key('child_story', int(id))
+    key = client.key('child_story', name)
     return client.get(key)
 
 def update_entries(story_entry):
@@ -56,7 +62,7 @@ def init_story_head(story_list, author, title, text):
     story_list['time'] = datetime.datetime.now()
     story_list['is_finished'] = False
     story_list['total_votes'] = 0
-    story_list['child_id'] = 0
+    story_list['child_id'] = ""
     
 def init_story_child(new_story, parent_story, author, text):
     new_story['author'] = author
@@ -64,6 +70,6 @@ def init_story_child(new_story, parent_story, author, text):
     new_story['time'] = datetime.datetime.now()
     new_story['is_finished'] = False
     new_story['total_votes'] = 0
-    new_story['parent_id'] = parent_story.id
-    new_story['child_id'] = 0
-    parent_story['child_id'] = new_story.id
+    new_story['parent_id'] = parent_story.key.id_or_name
+    new_story['child_id'] = ""
+    parent_story['child_id'] = new_story.key.id_or_name
