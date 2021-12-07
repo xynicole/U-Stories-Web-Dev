b3251bace55c26abe54ac491f722c9c35980d357
Shane Josapak <shjosapak@gmail.com>
2021-12-07 01:18:58 +0000

    added delete options

--- static-files-folder/js/stop-story-enabler.js
+++ static-files-folder/js/stop-story-enabler.js
@@ -2,14 +2,28 @@ let stopper = document.getElementById("stopper");
 let confirmer = document.getElementById("confirm-stop");
 let author = document.getElementById("author");
 let username = document.getElementById("user");
+let hider1 = document.getElementById("hider-if-stopped1");
+let hider2 = document.getElementById("hider-if-stopped2");
+let deleteInit = document.getElementById("init-delete");
+let confirmDelete = document.getElementById("confirm-delete");
 
 function stopAllower() {
     // console.log("Username: " + username.innerText + " Author: " + author.innerText);
     if (username.innerText == author.innerText) {
         stopper.removeAttribute("hidden");
+        deleteInit.removeAttribute("hidden");
     }
 }
 
-function unhideConfirm() {
+function unhideConfirmStop() {
     confirmer.removeAttribute("hidden");
+}
+
+function unhideConfirmDelete() {
+    confirmDelete.removeAttribute("hidden");
+}
+
+function hider() {
+    hider1.style.display = "none";
+    hider2.style.display = "none";
 }
